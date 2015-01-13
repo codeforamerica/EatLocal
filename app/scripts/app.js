547daf29d7ce9f04a1aa22c27bbefbb0936a0de8
Grant Smith <grantrobertsmith@gmail.com>
2015-01-13 00:33:12 -0800

    initial inclusion of leaflet stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,4 @@
  * Main module of the application.
  */
 angular
-  .module('eatlocalApp', ['ui.bootstrap']);
+  .module('eatlocalApp', []);
---
+++ app/scripts/main.js
@@ -0,0 +1,25 @@
+/*jslint browser: true*/
+/*global L */
+
+(function (window, document, L, undefined) {
+	'use strict';
+
+	L.Icon.Default.imagePath = 'images/';
+
+	/* create leaflet map */
+	var map = L.map('map', {
+		center: [52.5377, 13.3958],
+		zoom: 4
+	});
+
+	/* add default stamen tile layer */
+	new L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
+		minZoom: 0,
+		maxZoom: 18,
+		attribution: 'Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'
+	}).addTo(map);
+
+
+	L.marker([52.5, 13.4]).addTo(map);
+
+}(window, document, L));
