48d81980751a1131e70f93452f3e45d433a03f8c
Imanol Aranzadi <iiaranzadi@gmail.com>
2015-01-13 23:02:09 -0800

    added code for doing search

--- app/eatlocal.js
+++ app/eatlocal.js
@@ -2,5 +2,9 @@
     'use strict';
 
     var app = angular.module('EatLocal', []);
-}());
 
+    app.controller('MainCtrl', function($scope){
+      $scope.colors = ['Blue Cadinal', 'Green Roof', 'Papaya Whit', 'Slate Gray', 'Ocean Breeze', 'Yellow Lamp'];
+    });
+
+}());
